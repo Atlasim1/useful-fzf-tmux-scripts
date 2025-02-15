4a56181042ba8aa941d60be91695270eaadefa61
Atlasim <simondelorme4@gmail.com>
2025-02-15 17:20:49 -0500

    Fixed cheat sheet viewer
    fixed colors and other fixes

--- fzfcheatsheet.sh
+++ fzfcheatsheet.sh
@@ -1,3 +1,2 @@
-selection=`curl -s cht.sh/:list | fzf --preview='curl -s cht.sh/{} | bat -n --color=always' --tmux 90% --color=fg:white,hl:blue,fg+:white,bg+:black,hl+:blue,info:gray,prompt:cyan,pointer:cyan,marker:green,spinner:magenta,header:green --prompt "❯" --header='Cheatsheet Helper'`
-curl -s cht.sh/$selection | bat -p --paging=always
-
+selection=`curl -s cht.sh/:list | fzf --preview='curl -s cht.sh/{} | bat -n --color=always' --tmux 90% --color=fg:white,hl:blue,fg+:white,bg+:black,hl+:blue,info:gray,prompt:cyan,pointer:cyan,marker:green,spinner:magenta,header:green,border:gray --prompt "❯ " --header='Cheatsheet Helper'`
+tmux popup -w 90% -h 90% -S fg=brightblack -b rounded -E "curl -s cht.sh/$selection | bat -p --paging=always"
