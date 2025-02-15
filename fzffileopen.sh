6dd3f2b6261fef5e26b9bad951b43eba0c92cce5
Atlasim <simondelorme4@gmail.com>
2025-02-15 17:21:19 -0500

    fix opening nvim when canceled
    fixed nvim opening empty file when to file selected

--- fzffileopen.sh
+++ fzffileopen.sh
@@ -1,2 +1,7 @@
-(find . -type f | cut -b '3-') | fzf --color=fg:white,hl:blue,fg+:white,bg+:black,hl+:blue,info:gray,prompt:cyan,pointer:cyan,marker:green,spinner:magenta,header:green --tmux 70% \
-    --preview='bat -n --color=always --style=numbers {}' --header='Select file to edit' | xargs tmux neww nvim
+choiche=`(find . -type f | cut -b '3-') | fzf --color=fg:white,hl:blue,fg+:white,bg+:black,hl+:blue,info:gray,prompt:cyan,pointer:cyan,marker:green,spinner:magenta,header:green --tmux 70% \
+    --preview='bat -n --color=always --style=numbers {}' --header='Select file to edit'`
+
+if [[ -n $choiche ]]
+then
+    tmux neww nvim $choiche
+fi
